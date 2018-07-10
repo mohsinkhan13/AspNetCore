6c5eb59973f8e087366001f2b4a0f90999671818
mohsinkhan13 <mohsink13@gmail.com>
2018-07-10 11:28:11 +0100

    Initial files via upload

---
+++ WebApi/CityInfo.Api/Controllers/CitiesController.cs
@@ -0,0 +1,34 @@
+﻿using CityInfo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/Cities")]
+    public class CitiesController : Controller
+    {
+        List<City> _cities = new List<City>
+            {
+                new City{Id=1, Name="London"  , Description = "Some description"},
+                new City{Id=2, Name="New York", Description = "Some description"},
+                new City{Id=3, Name="Mumbai"  , Description = "Some description"},
+                new City{Id=4, Name="Prague"  , Description = "Some description"}
+            };
+
+        public ActionResult GetCities()
+        {
+            return new JsonResult(_cities);
+        }
+
+        [Route("{id}")]
+        public ActionResult GetCity(int id)
+        {
+            var result = _cities.FirstOrDefault(x => x.Id == id);
+            if (result == null)
+                return new NotFoundResult();
+
+            return new JsonResult(result);
+        }
+    }
+}
